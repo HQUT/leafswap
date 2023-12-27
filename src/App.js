0f10bf42670cd06ad85d3f419025db65265f06b6
hibbel996 <hibbel@kth.se>
2023-12-27 19:08:21 +0100

    app uppdate

--- src/App.js
+++ src/App.js
@@ -8,33 +8,38 @@ import { HomePresenter } from './Presenter/homePresenter';
 import { About } from './views/About';
 import SidebarComp from './js/komponenter/Sidebar';
 import { LoginPresenter } from './Presenter/loginPresenter';
+import { BookCollectionPresenter } from './Presenter/bookCollectionPresenter';
 import PersonalProfile from './js/komponenter/profile';
+import { ProfileProvider } from './js/komponenter/ProfileContext';
 
 function App(props) {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
-    <BrowserRouter>
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-md-3">
-            <SidebarComp isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
-          </div>
-          <div className="col-md-9">
-            <Routes>
-            <Route path="/home" element={<HomePresenter />} />
-              <Route path="/profile" element={<PersonalProfile />} />
-              <Route path="/login" element={<LoginPresenter setIsLoggedIn={setIsLoggedIn} />} />
-              <Route path="/about" element={<About model={props.model} />} />
-              <Route path="/search" element={<SearchPresenter model={props.model} />} />
-              <Route path="/details" element={<DetailsPresenter model={props.model} />} />
-              <Route index path="*" element={<HomePresenter model={props.model} />} />
-            </Routes>
+    <ProfileProvider> 
+      <BrowserRouter>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-3">
+              <SidebarComp isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+            </div>
+            <div className="col-md-9">
+              <Routes>
+                <Route path="/home" element={<HomePresenter />} />
+                <Route path="/profile" element={<PersonalProfile />} />
+                <Route path="/login" element={<LoginPresenter setIsLoggedIn={setIsLoggedIn} />} />
+                <Route path="/about" element={<About model={props.model} />} />
+                <Route path="/search" element={<SearchPresenter model={props.model} />} />
+                <Route path="/collection" element={<BookCollectionPresenter model={props.model} />} />
+                <Route path="/details" element={<DetailsPresenter model={props.model} />} />
+                <Route index path="*" element={<HomePresenter model={props.model} />} />
+              </Routes>
+            </div>
           </div>
         </div>
-      </div>
-    </BrowserRouter>
+      </BrowserRouter>
+    </ProfileProvider>
   );
 }
 
-export default App;
+export default App;
