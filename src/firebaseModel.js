2a1b49be8722a467cb516f64f9081a8a4fad6a60
hibbel996 <hibbel@kth.se>
2023-12-27 18:56:05 +0100

    uppdate firebasee model

--- src/firebaseModel.js
+++ src/firebaseModel.js
@@ -1,43 +1,34 @@
-// you will find 2 imports already there, add the configuration and instantiate the app and database:
-import app from "./firebaseConfig.js";
-import { getDatabase, ref, get, set } from "firebase/database";
-import { onAuthStateChanged } from "firebase/auth"; 
-const db= getDatabase(app);
-const rf= ref(db, PATH+"/"+model.user.uid);
-const PATH="LeafSwap";
+import { getDatabase, ref, update,  get, onValue} from "firebase/database";
+import {app} from './firebaseConfig';
 
-function modelToPersistence(model) {
-    return {}
+
+export function PersistModel(model) {
+    if (!model.user) return;
+    const db = getDatabase(app); 
+    const userRef = ref(db, 'users/' + model.user.uid);
+    onValue(userRef, (snapshot) => {
+        const data = snapshot.val();
+        if (data) {
+            model.setBooks(data.books || []);
+            model.collections = data.collections || {};
+            model.notifyObservers();
+        }
+    }, {
+        onlyOnce: true 
+    });
 }
 
-function persistenceToModel(data, model) {
-
-}
-
-function readFromFirebase(model) {
-    model.ready = false;
-    if(model.user) {
-        get(rf).then(readyACB).then(e=>model.ready = true);
-        function readyACB(snap) {return persistenceToModel(snap.val(), model)}
-    }
-}
-
-function saveToFirebase(model) {
-    if(model.ready) {
-        set(rf, modelToPersistence(model));
-    }
-}
-
-function connectToFirebase(model, watchFunction) {
-    readFromFirebase(model);
-    function modelChangeWatchACB() {
-        return [];
-    }
-    function sideEffectACB(data) {
-        saveToFirebase(model);
-    }
-    watchFunction(modelChangeWatchACB, sideEffectACB);
-}
-
-export {modelToPersistence, persistenceToModel, saveToFirebase, readFromFirebase};
-export default connectToFirebase;
+export function PersistUpdate(model) {
+    if(!model.user) return;
+    const db = getDatabase(app); 
+    update(ref(db, 'users/'+ model.user.uid), {
+      books: model.books,
+      collections: model.collections
+  })
+  .then(()=>{
+    return;
+  })
+  .catch((error)=>{
+    console.error(error); 
+  });
+  }
